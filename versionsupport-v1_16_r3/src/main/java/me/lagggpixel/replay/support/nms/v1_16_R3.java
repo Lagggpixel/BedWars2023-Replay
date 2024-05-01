195302b4297b9e8c21cdc7f21476f4b88c4e6e44
Wanbin Cao <lagggpixel@gmail.com>
2024-05-01 13:19:10 +0100

    Fixed 1.16.3 support

--- versionsupport-v1_16_r3/src/main/java/me/lagggpixel/replay/support/nms/v1_16_R3.java
+++ versionsupport-v1_16_r3/src/main/java/me/lagggpixel/replay/support/nms/v1_16_R3.java
@@ -2,16 +2,5 @@ package me.lagggpixel.replay.support.nms;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class v1_16_R3 extends JavaPlugin {
-
-    @Override
-    public void onEnable() {
-        // Plugin startup logic
-
-    }
-
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
+public final class v1_16_R3 {
 }
